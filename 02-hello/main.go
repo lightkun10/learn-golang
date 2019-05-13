b28ad33b7c9bd5c051617479c7479c39a6a5a630
@lightkun12 <lifeisubw@gmail.com>
2019-05-13 14:54:12 +0700

    You're doing great

--- 02-hello/main.go
+++ 02-hello/main.go
@@ -1,27 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello...")
-
-	foo()
-
-	fmt.Println("Something more")
-
-	for i := 0; i < 40; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
-	}
-
-	bar()
-}
-
-func foo() {
-	fmt.Println("I'm in foo!")
-}
-
-func bar() {
-	fmt.Println("exit...")
+	n, err := fmt.Println("Pandu Panda", 24, true)
+	fmt.Println(n)
+	fmt.Println(err)
 }
---
+++ 03-packages/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42
+	fmt.Println(x)
+	x = 33
+	fmt.Println(x)
+	y := 100 + 20
+	fmt.Println(y)
+}
